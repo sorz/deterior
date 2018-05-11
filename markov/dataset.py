4b6b7fac5a9ebe9d2aede4cdb2e1f17e0a658b00
sorz <me@sorz.org>
2018-05-11 22:45:02 +1000

    Refactor dataset loading

--- markov/dataset.py
+++ markov/dataset.py
@@ -1,7 +1,6 @@
 from collections import namedtuple, defaultdict
 from csv import DictReader
 from datetime import datetime
-import re
 import sys
 
 
@@ -11,7 +10,7 @@ COLUMN_S1 = 'Next State'
 COLUMN_TIME = 'Time'
 
 Record = namedtuple('Record', ['s0', 's1', 't'])
-
+Inspect = namedtuple('Inspect', ['id', 'state', 'date'])
 
 def load_transit_log(path: str) -> ([Record], int):
     records = []
@@ -22,43 +21,52 @@ def load_transit_log(path: str) -> ([Record], int):
             s1 = int(row[COLUMN_S1])
             t = int(row[COLUMN_TIME])
             records.append(Record(s0, s1, t))
-            max_state = max(max_state, state)
+            max_state = max(max_state, s0, s1)
     return records, max_state
 
 
 def load_inspect_log(path: str, date_format='%Y-%m-%d') -> ([Record], int):
-    states = set()
-    id_inspects = defaultdict(list)
+    inpsects = []
     with open(path, newline='') as csv:
         csv = DictReader(csv)
         for row in csv:
-            rid = row[COLUMN_ID]
+            sid = row[COLUMN_ID]
             state = row[COLUMN_S0]
             date = row[COLUMN_TIME]
-            if not rid:
+            if not sid:
                 raise ValueError(f'blank ID in row {csv.line_num}')
             if not state or not date:
-                print(f'In row {csv.line_num}, {rid}'
-                       'has blank state or time, ignored.', file=sys.stderr)
+                print(f'In row {csv.line_num}, {id} '
+                      'has blank state or time, ignored.', file=sys.stderr)
                 continue
             date = datetime.strptime(date, date_format)
-            id_inspects[rid].append((state, date))
-            states.add(state)
-    print(f'{sum(map(len, id_inspects.values()))} records loaded')
+            inpsects.append(Inspect(sid, state, date))
+    print(f'{len(inpsects)} inspection records loaded')
+    return _inpsects_to_records(inpsects)
+
+
+def _inpsects_to_records(inspects: [Inspect]) -> ([Record], int):
+    """Return list of records and the total number of states."""
+    states = set()
+    id_inspects = defaultdict(list)
+    for sid, state, date in inspects:
+        states.add(state)
+        id_inspects[sid].append((state, date))
+
     states = sorted(states)
-    smap = { s: n for n, s in enumerate(states) }
-    for s in states:
-        if s != str(smap[s]):
-            print(f'Mapping state "{s}" to S{smap[s]}')
+    smap = {s: n for n, s in enumerate(states)}
+    for state in states:
+        if state != str(smap[state]):
+            print(f'Mapping state "{state}" to S{smap[state]}')
     print(f'Found {len(smap)} states in total')
 
     records = []
-    for inspects in id_inspects.values():
-        if len(inspects) < 2:
+    for states_dates in id_inspects.values():
+        if len(states_dates) < 2:
             continue
-        inspects = sorted(inspects)
-        for i in range(len(inspects) - 1):
-            (s0, t0), (s1, t1) = inspects[i], inspects[i+1]
+        states_dates = sorted(states_dates)
+        for i in range(len(states_dates) - 1):
+            (s0, t0), (s1, t1) = states_dates[i], states_dates[i+1]
             days = (t1 - t0).days
             if days <= 0:
                 continue
